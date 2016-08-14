99543f6414c8171761c6ebbe553e7739adcc885e
Cheri <avyishi@gmail.com>
2016-08-13 19:42:51 -0700

    Fixed navbar space issue

--- src/components/App.js
+++ src/components/App.js
@@ -12,36 +12,32 @@ const App = React.createClass({
   },
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-xs-12">
-            <nav className="navbar navbar-default navbar-fixed-top">
-              <div className="container-fluid">
-                <div className="navbar-header">
-                  <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                  </button>
-                  <a className="navbar-brand" href="/">Read and Watch</a>
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">Read and Watch</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <form className="navbar-form navbar-left">
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Search" onChange={this.update}/>
                 </div>
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <form className="navbar-form navbar-left">
-                    <div className="form-group">
-                      <input type="text" className="form-control" placeholder="Search" onChange={this.update}/>
-                    </div>
-                    <Link to={"/search/"+this.state.text} ><button className="btn btn-default">Search</button></Link>
-                  </form>
-                  <ul className="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
-            {this.props.children}
+                <Link to={"/search/"+this.state.text} ><button className="btn btn-default">Search</button></Link>
+              </form>
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+              </ul>
+            </div>
           </div>
-        </div>
+        </nav>
+        {this.props.children}
       </div>
     );
   }
